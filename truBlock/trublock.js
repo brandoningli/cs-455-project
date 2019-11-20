0f94853c18083a9cfe09b40d24c900e5d9eafadc
hypeincarnate <54373465+hypeincarnate@users.noreply.github.com>
2019-11-20 17:51:55 -0600

    Update trublock.js

--- truBlock/trublock.js
+++ truBlock/trublock.js
@@ -9,4 +9,11 @@ function toggleEnabled() {
 }
 
 /* BLOCKING CODE GOES HERE */
+I think this is where the filter list is suppose to go
+var pattern = "https://mdn.mozillademos.org/*";
 
+browser.webRequest.onBeforeRequest.addListener(
+  //log redirect or modify request headers
+  {urls:[pattern]},
+  ["blocking"]
+);
