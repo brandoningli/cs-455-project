88f59e519bba2df643f09345673ced2f83d5e1e7
hypeincarnate <54373465+hypeincarnate@users.noreply.github.com>
2019-11-20 17:47:04 -0600

    Update trublock.js

--- truBlock/trublock.js
+++ truBlock/trublock.js
@@ -9,22 +9,4 @@ function toggleEnabled() {
 }
 
 /* BLOCKING CODE GOES HERE */
-{
 
-  "description": "Demonstrating webRequests",
-  "manifest_version": 2,
-  "name": "webRequest-demo",
-  "version": "1.0",
-
-  "permissions": [
-    "webRequest",
-    "webRequestBlocking",
-    "https://developer.mozilla.org/",
-    "https://mdn.mozillademos.org/"
-  ],
- 
-  "background": {
-    "scripts": ["background.js"]
-  }
-
-}
